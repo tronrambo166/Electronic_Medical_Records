4d264ac78a83e5c1fd358cf72d9def244152fffd
Malan Español <tronrambo166@gmail.com>
2024-03-29 14:51:43 +0600

    update

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ define('LARAVEL_START', microtime(true));
 | If the application is maintenance / demo mode via the "down" command we
 | will require this file so that any prerendered template can be shown
 | instead of starting the framework, which could cause an exception.
-|
 */
 
 if (file_exists(__DIR__.'/core/storage/framework/maintenance.php')) {
